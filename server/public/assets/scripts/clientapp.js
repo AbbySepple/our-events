4db12e3f5b2944d1488f2942f73ec9fe93f77cf1
Abby Sepple <abby.sepple@gmail.com>
2017-06-08 16:31:50 -0500

    MVP

--- server/public/assets/scripts/clientapp.js
+++ server/public/assets/scripts/clientapp.js
@@ -49,16 +49,6 @@ myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $l
 // below are angular meterial provided code. change 'AppCtrl' on new ones so they
 // don't interfear with eachother
 
-//this is for the tab bar. privided by materials angular
-myApp.controller('TabCtrl', function(){
-
-  var vm = this;
-function AppCtrl() {
-vm.currentNavItem = '/';
-}
-console.log('inside TabCtrl');
-});
-
 //this is for the daste picker. provided by materials angular
 myApp.controller('AppCtrl', function() {
   this.myDate = new Date();
